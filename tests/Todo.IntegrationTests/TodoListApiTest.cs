b1ebfc0bfd9d89da7196f27f2246ba2dad7a4b44
Sergey Shambir <sergey.shambir@ispring.ru>
2024-08-01 18:54:07 +0300

    Полностью переписал один из интеграционных тестов на Specflow

--- tests/Todo.IntegrationTests/TodoListApiTest.cs
+++ tests/Todo.IntegrationTests/TodoListApiTest.cs
@@ -10,47 +10,6 @@ public class TodoListApiTest(IntegrationTestFixture<Program> fixture) : IClassFi
     private readonly TodoListTestApiGateway _gateway = new(fixture.HttpClient);
 
     [Fact]
-    public async Task TestCreateEditDeleteSingleList()
-    {
-        TodoListDetailedData list = await _gateway.CreateTodoList("Home");
-        Assert.Equal("Home", list.Name);
-
-        await _gateway.AddTodoItem(list.Id, "Clean the room");
-        await _gateway.AddTodoItem(list.Id, "Eat borscht");
-        await _gateway.AddTodoItem(list.Id, "Make borscht");
-        await _gateway.AddTodoItem(list.Id, "Feed the hamster");
-
-        list = await _gateway.GetTodoList(list.Id);
-        Assert.Equal(["Clean the room", "Eat borscht", "Make borscht", "Feed the hamster"], GetItemTitles(list));
-
-        await _gateway.EditTodoItem(list.Id, 3, new EditTodoItemParams(Position: 2));
-
-        list = await _gateway.GetTodoList(list.Id);
-        Assert.Equal(["Clean the room", "Eat borscht", "Feed the hamster", "Make borscht"], GetItemTitles(list));
-
-        await _gateway.EditTodoItem(list.Id, 1, new EditTodoItemParams(Position: 3, Title: "Eat borscht with soup cream"));
-
-        list = await _gateway.GetTodoList(list.Id);
-        Assert.Equal(["Clean the room", "Feed the hamster", "Make borscht", "Eat borscht with soup cream"], GetItemTitles(list));
-        Assert.Equal([], GetCompletedItemPositions(list));
-
-        await _gateway.EditTodoItem(list.Id, 0, new EditTodoItemParams(IsCompleted: true));
-        await _gateway.EditTodoItem(list.Id, 2, new EditTodoItemParams(IsCompleted: true));
-
-        list = await _gateway.GetTodoList(list.Id);
-        Assert.Equal(["Clean the room", "Feed the hamster", "Make borscht", "Eat borscht with soup cream"], GetItemTitles(list)); // nothing changed
-        Assert.Equal([0, 2], GetCompletedItemPositions(list));
-
-        await _gateway.DeleteTodoItem(list.Id, 1);
-
-        list = await _gateway.GetTodoList(list.Id);
-        Assert.Equal(["Clean the room", "Make borscht", "Eat borscht with soup cream"], GetItemTitles(list));
-        Assert.Equal([0, 1], GetCompletedItemPositions(list));
-
-        await _gateway.DeleteTodoList(list.Id);
-    }
-
-    [Fact]
     public async Task TestCreateEditDeleteTwoLists()
     {
         TodoListDetailedData todoHome = await _gateway.CreateTodoList("Home");
--- tests/Todo.Specs/Steps/TodoListStepDefinitions.cs
+++ tests/Todo.Specs/Steps/TodoListStepDefinitions.cs
@@ -25,38 +25,75 @@ public sealed class TodoListStepDefinitions(TestServerDriver driver)
         get => _lastTodoList ?? throw new InvalidOperationException("Список дел ещё не открыт");
     }
 
-    [Given(@"я создал список ""(.*)""")]
+    [Given(@"(?:я )?создал список ""(.*)""")]
     public async Task ПустьЯСоздалСписок(string name)
     {
         var list = await _gateway.CreateTodoList(name);
         _lastListId = list.Id;
     }
 
-    [Given(@"добавил задачу ""(.*)""")]
+    [Given(@"(?:я )?добавил задачу ""(.*)""")]
     public async Task ПустьЯДобавилЗадачу(string title)
     {
         await _gateway.AddTodoItem(LastListId, title);
     }
 
-    [When("я открыл созданный список")]
+    [Given(@"(?:я )?переместил задачу №(.+) на позицию №(.+)")]
+    public async Task ПустьЯПереместилЗадачу(int position, int newPosition)
+    {
+        await _gateway.EditTodoItem(LastListId, position, new EditTodoItemParams(Position: newPosition));
+    }
+
+    [Given(@"(?:я )?переименовал задачу №(.+) на ""(.+)""")]
+    public async Task ПустьЯПереименовалЗадачуНа(int position, string newTitle)
+    {
+        await _gateway.EditTodoItem(LastListId, position, new EditTodoItemParams(Title: newTitle));
+    }
+
+    [Given("(?:я )?завершил задачу №(.+)")]
+    public async Task ПустьЯЗавершилЗадачу(int position)
+    {
+        await _gateway.EditTodoItem(LastListId, position, new EditTodoItemParams(IsCompleted: true));
+    }
+
+    [Given("(?:я )?удалил задачу №(.+)")]
+    public async Task ПустьЯУдалилЗадачу(int position)
+    {
+        await _gateway.DeleteTodoItem(LastListId, position);
+    }
+
+    [When("(?:я )?открыл созданный список")]
     public async Task КогдаЯОткрылСозданныйСписок()
     {
         _lastTodoList = await _gateway.GetTodoList(LastListId);
     }
 
-    [Then(@"вижу задачи: ""(.*)""")]
+    [Then(@"(?:я )?вижу задачи: ""(.*)""")]
     public void ТогдаЯВижуЗадачи(string tasksTitlesCommaSeparated)
     {
-        string[] taskTitles = tasksTitlesCommaSeparated
-            .Split(",")
-            .Select(x => x.Trim())
-            .ToArray();
-
+        string[] taskTitles = SplitCommaSeparatedList(tasksTitlesCommaSeparated);
         Assert.Equal(taskTitles, GetItemTitles(LastTodoList));
+    }
+
+    [Then(@"(?:я )?вижу завершённые задачи: ""(.*)""")]
+    public void ТогдаЯВижуЗавершённыеЗадачи(string tasksTitlesCommaSeparated)
+    {
+        string[] taskTitles = SplitCommaSeparatedList(tasksTitlesCommaSeparated);
+        Assert.Equal(taskTitles, GetCompletedItemTitles(LastTodoList));
+    }
+
+    private static string[] SplitCommaSeparatedList(string text)
+    {
+        return text.Split(",").Select(x => x.Trim()).Where(x => x != "").ToArray();
     }
 
     private static string[] GetItemTitles(TodoListDetailedData list)
     {
         return list.Items.Select(item => item.Title).ToArray();
+    }
+
+    private static string[] GetCompletedItemTitles(TodoListDetailedData list)
+    {
+        return list.Items.Where(item => item.IsComplete).Select(item => item.Title).ToArray();
     }
 }
